effa8477c7c92c90d83caa1d6e5118e282952f46
bhkim <bh2888#nate.com>
2018-10-12 17:56:56 +0900

    test commit

--- src/qt/test/uritests.cpp
+++ src/qt/test/uritests.cpp
@@ -8,55 +8,55 @@ void URITests::uriTests()
 {
     SendCoinsRecipient rv;
     QUrl uri;
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?req-dontexist="));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?req-dontexist="));
     QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?dontexist="));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?dontexist="));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
-    QVERIFY(rv.address == QString("LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9"));
+    QVERIFY(rv.address == QString("LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi"));
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 0);
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?label=Wikipedia Example Address"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?label=Wikipedia Example Address"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
-    QVERIFY(rv.address == QString("LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9"));
+    QVERIFY(rv.address == QString("LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi"));
     QVERIFY(rv.label == QString("Wikipedia Example Address"));
     QVERIFY(rv.amount == 0);
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?amount=0.001"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?amount=0.001"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
-    QVERIFY(rv.address == QString("LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9"));
+    QVERIFY(rv.address == QString("LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi"));
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 100000);
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?amount=1.001"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?amount=1.001"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
-    QVERIFY(rv.address == QString("LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9"));
+    QVERIFY(rv.address == QString("LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi"));
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 100100000);
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?amount=100&label=Wikipedia Example"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?amount=100&label=Wikipedia Example"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
-    QVERIFY(rv.address == QString("LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9"));
+    QVERIFY(rv.address == QString("LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi"));
     QVERIFY(rv.amount == 10000000000LL);
     QVERIFY(rv.label == QString("Wikipedia Example"));
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?message=Wikipedia Example Address"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?message=Wikipedia Example Address"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
-    QVERIFY(rv.address == QString("LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9"));
+    QVERIFY(rv.address == QString("LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi"));
     QVERIFY(rv.label == QString());
 
-    QVERIFY(GUIUtil::parseBitcoinURI("nexcoin://LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?message=Wikipedia Example Address", &rv));
-    QVERIFY(rv.address == QString("LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9"));
+    QVERIFY(GUIUtil::parseBitcoinURI("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?message=Wikipedia Example Address", &rv));
+    QVERIFY(rv.address == QString("LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi"));
     QVERIFY(rv.label == QString());
 
     // We currently don't implement the message parameter (ok, yea, we break spec...)
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?req-message=Wikipedia Example Address"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?req-message=Wikipedia Example Address"));
     QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?amount=1,000&label=Wikipedia Example"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?amount=1,000&label=Wikipedia Example"));
     QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
 
-    uri.setUrl(QString("nexcoin:LQDPC5rbjDB72fGFVHu4enYhxGAZuRiFh9?amount=1,000.0&label=Wikipedia Example"));
+    uri.setUrl(QString("nexcoin:LKFJvEvbAv6sNUZy6mrnfWLv8PoDRZBzqi?amount=1,000.0&label=Wikipedia Example"));
     QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
 }
